66ffd13d562fe1f1c68c4ff8345a4ce725675f95
mahdirahmani1376 <rahmanimahdi20@yahoo.com>
2022-10-21 15:12:10 +0330

    4

--- app/Http/Requests/StoreproductRequest.php
+++ app/Http/Requests/StoreproductRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Language;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreproductRequest extends FormRequest
@@ -23,10 +24,19 @@ class StoreproductRequest extends FormRequest
      */
     public function rules()
     {
+        $count_of_products = Language::count();
+        
         return [
-            'data'                  => ['required', 'array'],
-            'data.*.language_id'    => ['required', 'numeric'],
-            'data.*.name'           => ['required', 'string'],
+            'data'                   =>  ['required', 'array','min:$count_of_products','max:$count_of_products'],
+            'data.*.language_id'     =>  ['required', 'numeric'],
+            'data.*.name'            =>  ['required', 'string'],
+            'data.*.model'            =>  ['required','string'],
+            'data.*.name'             =>  ['required','string'],
+            'data.*.meta_title'       =>  ['nullable','max:100'],
+            'data.*.meta_description' =>  ['nullable','max:190'],
+            'data.*.meta_keywords'    =>  ['nullable','max:50'],
+            'data.*.canonical'        =>  ['nullable'],
+            'data.*.description'      =>  ['required','max:300'],
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/product',[ProductController::class,'index']);
-Route::post('/product',[ProductController::class,'store']);
+Route::apiResource('/product',ProductController::class);
